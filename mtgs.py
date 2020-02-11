3c42fd5d131a342d5467351809474241a0995963
Robby Zar <robbyzar@RobbyZ2019-Mac.local>
2020-02-10 20:01:18 -0600

    Export to csv

--- mtgs.py
+++ mtgs.py
@@ -1,4 +1,6 @@
 import json
+import time
+import pandas as pd
 import requests
 
 def main():
@@ -11,25 +13,35 @@ def main():
         else:
             new_card = build_card(query)
             final_list.append(new_card)
-    print(final_list)
+
+    df = pd.DataFrame(final_list)
+    # Get current time
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+    # Export final list to csv
+    df.to_csv("data/export_{}.csv".format(timestr))
+    print(df)
 
 def get_input():
+    """Get user input"""
     return(input("Enter card name (q for quit): "))
 
 def get_request(card):
+    """Take user input and make API request"""
     r = requests.get('https://api.scryfall.com/cards/search?q={}'.format(card))
     r_text = r.text
     r_json = json.loads(r_text)
     return(r_json["data"][0])
 
 def get_atts(card):
+    """Return dict with name, set, and price of card"""
     card_name = card["name"]
     set_name = card["set_name"]
     prices = card["prices"]
     card_atts = {
         "name": card_name,
         "set": set_name,
-        "prices": prices
+        "price_normal": prices["usd"],
+        "price_foil": prices["usd_foil"]
     }
     print("Card Name: {}\nSet: {}".format(card_name, set_name))
     print("Normal: {}\nFoil: {}".format(prices["usd"], prices["usd_foil"]))
@@ -39,6 +51,7 @@ def build_card(q):
     card_request = get_request(q)
     atts = get_atts(card_request)
     return(atts)
+
 
 if __name__ == "__main__":
     main()
