b071c76c1f00a4338259b38b0a060d6d7516353a
elctrc <electricexperiment@gmail.com>
2020-02-11 22:18:21 -0600

    Added some exception logic

--- mtgs.py
+++ mtgs.py
@@ -1,9 +1,14 @@
 import json
+import logging
 import time
 import pandas as pd
 import requests
 
 def main():
+    # Setup logging
+    # lformat = '%(asctime)-15s %(clientip)s %(user)-8s %(message)s'
+    logging.basicConfig()
+
     end_query = False
     final_list = []
     while end_query == False:
@@ -11,8 +16,13 @@ def main():
         if query == "q":
             end_query = True
         else:
-            new_card = build_card(query)
-            final_list.append(new_card)
+            # new_card = build_card(query)
+            card_request = get_request(query)
+            if card_request == "Error":
+                pass
+            else:
+                new_card = get_atts(card_request)
+                final_list.append(new_card)
 
     df = pd.DataFrame(final_list)
     # Get current time
@@ -30,7 +40,11 @@ def get_request(card):
     r = requests.get('https://api.scryfall.com/cards/search?q={}'.format(card))
     r_text = r.text
     r_json = json.loads(r_text)
-    return(r_json["data"][0])
+    try:
+        return(r_json["data"][0])
+    except KeyError:
+        logging.warning("Card not found")
+        return("Error")
 
 def get_atts(card):
     """Return dict with name, set, and price of card"""
@@ -47,10 +61,9 @@ def get_atts(card):
     print("Normal: {}\nFoil: {}".format(prices["usd"], prices["usd_foil"]))
     return(card_atts)
 
-def build_card(q):
-    card_request = get_request(q)
-    atts = get_atts(card_request)
-    return(atts)
+# def build_card(q):
+#     card_request = get_request(q)
+#     return(get_atts(card_request))
 
 
 if __name__ == "__main__":
@@ -60,4 +73,4 @@ if __name__ == "__main__":
 # Pretty print results
 # Store results in text file
 # More fuzzy searching, return multiple results
-
+# Send to Google Sheets
